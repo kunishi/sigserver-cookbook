f8ef5b0291f4cd3cd4b3176cf2c153e56d755f16
Takeo Kunishima <t.kunishi@gmail.com>
2014-07-31 14:15:23 +0900

    add a new cookbook for xerces-c.

--- Vagrantfile
+++ Vagrantfile
@@ -102,6 +102,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     chef.add_recipe "build-tools"
     chef.add_recipe "java"
     chef.add_recipe "opengl"
+    chef.add_recipe "xerces-c"
 
     chef.json = {
       tz: "Asia/Tokyo",
---
+++ site-cookbooks/xerces-c/metadata.rb
@@ -0,0 +1,8 @@
+name             'xerces-c'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures xerces-c'
+long_description 'Installs/Configures xerces-c'
+version          '0.1.0'
+
